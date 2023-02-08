e7959c923e03b681989f270535adb658043e2bf4
Tom Jefferis <10071295+tomjefferis@users.noreply.github.com>
2023-02-08 13:46:38 +0000

    update

--- eegnet.py
+++ eegnet.py
@@ -18,7 +18,7 @@ def get_data():
     data = []
     scores = []
     order = []
-    for file in [f for f in os.listdir("../EEG/") if f.endswith(".fif") and not f.endswith("resting.fif") and not f.endswith("hard.fif")]:
+    for file in [f for f in os.listdir("../EEG/") if f.endswith(".fif") and not f.endswith("resting.fif")]: # and not f.endswith("hard.fif")]:
         filepath = "../EEG/" + file
         print(file)
         # get number from file name
@@ -51,7 +51,7 @@ def remove_participant(data, scores, order, participant):
             removed_participant.append(x)
             removed_scores.append(y)
             
-    return newdata, newscores, removed_participant, removed_scores
+    return np.array(newdata), np.array(newscores), np.array(removed_participant), np.array(removed_scores)
 
 
 
@@ -81,50 +81,50 @@ def remove_nan(array,scores,order):
     order = order[~mask]
     return array, scores, order
 
-window_size = 2500
-
-X, Y, order = get_data()
-X, Y = split_data(X, Y, window_size=window_size)
-
-length = len(X)
-X = np.array(X)
-X = X.reshape(length, 32, window_size, 1)
-#X = np.moveaxis(X, [3], [1])
-Y = np.array(Y)
-#remove nan values
-X, Y = remove_nan(X, Y)
-
-X -= np.min(X)
-X /= np.max(X)
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-
-model = EEGNet_seq_attention(2, 32, window_size, loss='sparse_categorical_crossentropy', dropoutType='SpatialDropout2D', learning_rate=0.001)
-
-model.fit(X_train, Y_train, epochs=500, batch_size=32, validation_split=0.1)
-
-#save model
-model.save("eegnetbinary.h5")
-
-#evaluate model
-model.evaluate(X_test, Y_test)
-
-#plot confusion matrix 
-Y_pred = model.predict(X_test)
-Y_pred = np.argmax(Y_pred, axis=1)
-cm = confusion_matrix(Y_test, Y_pred)
-f, ax = plt.subplots(figsize=(8, 8))
-sns.heatmap(cm, annot=True, linewidths=0.01, cmap="Greens", linecolor="gray", fmt= '.1f', ax=ax)
-plt.xlabel("Predicted Label")
-plt.ylabel("True Label")
-plt.title("Confusion Matrix")
-plt.show()
-
-#plot loss graph
-plt.plot(model.history.history['loss'])
-plt.plot(model.history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+#window_size = 2500
+#
+#X, Y, order = get_data()
+#X, Y = split_data(X, Y, window_size=window_size)
+#
+#length = len(X)
+#X = np.array(X)
+#X = X.reshape(length, 32, window_size, 1)
+##X = np.moveaxis(X, [3], [1])
+#Y = np.array(Y)
+##remove nan values
+#X, Y = remove_nan(X, Y)
+#
+#X -= np.min(X)
+#X /= np.max(X)
+#
+#X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+#
+#model = EEGNet_seq_attention(2, 32, window_size, loss='sparse_categorical_crossentropy', dropoutType='SpatialDropout2D', learning_rate=0.001)
+#
+#model.fit(X_train, Y_train, epochs=500, batch_size=32, validation_split=0.1)
+#
+##save model
+#model.save("eegnetbinary.h5")
+#
+##evaluate model
+#model.evaluate(X_test, Y_test)
+#
+##plot confusion matrix 
+#Y_pred = model.predict(X_test)
+#Y_pred = np.argmax(Y_pred, axis=1)
+#cm = confusion_matrix(Y_test, Y_pred)
+#f, ax = plt.subplots(figsize=(8, 8))
+#sns.heatmap(cm, annot=True, linewidths=0.01, cmap="Greens", linecolor="gray", fmt= '.1f', ax=ax)
+#plt.xlabel("Predicted Label")
+#plt.ylabel("True Label")
+#plt.title("Confusion Matrix")
+#plt.show()
+#
+##plot loss graph
+#plt.plot(model.history.history['loss'])
+#plt.plot(model.history.history['val_loss'])
+#plt.title('model loss')
+#plt.ylabel('loss')
+#plt.xlabel('epoch')
+#plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
